3c2dd4767e5ac69a02c9623a51e8ffcbdb88b7ab
silver <dkfvktorcp@naver.com>
2022-03-15 23:06:14 +0900

    불필요한 파일 제거

--- app/bin/www.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require("../../app");
-const PORT = process.env.PORT || 3000; // PORT 환경변수가 있을때는 해당 포트에서 동작, 없으면 3000으로 적용
-
-app.listen(PORT, () => {
-    console.log(`${PORT} 포트에서 서버가 가동되었습니다.`);
-});
